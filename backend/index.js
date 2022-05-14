71a13ade5684174d8805132966179824e0cf256d
Jovaras Leonavicius <52199839+jowarnis@users.noreply.github.com>
2022-05-14 19:09:30 +0300

    trying to deploy node.js api to vercel

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { MongoClient } from "mongodb";
+// import { MongoClient } from "mongodb";
 dotenv.config();
 
 const connect = process.env.MONGO_CONNECTION_STRING;
-const mongoClient = new MongoClient(connect);
+// const mongoClient = new MongoClient(connect);
 const port = process.env.port;
 const APIkey = process.env.key;
 
@@ -16,15 +16,15 @@ app.use(cors());
 app.get("/:location", async (req, res) => {
   const cityname = req.params.location;
   console.log(cityname);
-  const connection = await mongoClient.connect();
+  // const connection = await mongoClient.connect();
   let fetchResponse = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIkey}`
   );
   let responseJSON = await fetchResponse.json();
-  await connection
-    .db("weatherApp")
-    .collection("weatherData")
-    .insertOne(responseJSON);
+  // await connection
+  //   .db("weatherApp")
+  //   .collection("weatherData")
+  //   .insertOne(responseJSON);
   const tempToC = (responseJSON.main.temp - 273.15).toFixed(1);
   const dataObj = { city: responseJSON.name, temp: tempToC };
   res.send(dataObj);
@@ -32,3 +32,4 @@ app.get("/:location", async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+module.exports = app;
